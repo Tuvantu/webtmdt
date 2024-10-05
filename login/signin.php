5cfeff30408682157b7e11ce1fdb888538449874
Tuvantu <tuvantu251202@gmail.com>
2024-10-05 10:49:34 +0700

    tu

--- login/signin.php
+++ login/signin.php
@@ -1,3 +1,16 @@
+<?php
+    include 'config.php';
+    
+    if (isset($_POST['submit'])) 
+    {
+        $user_name = mysqli_real_escape_string($conn, $_POST['name']);
+    }
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,12 +19,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng ký tài khoản</title>
     <link rel="stylesheet" href="../css/global.css">
-    <link rel="stylesheet" href="./css/signin.css">
+    <link rel="stylesheet" href="./css/signincss.css">
 </head>
 
 <body>
     <main>
-        <div class="main__content">
+        <form method="POST" class="main__content">
             <div class="signin__header">
 
             </div>
@@ -20,27 +33,32 @@
             </div>
 
             <div class="signin__input">
-                <input type="text" placeholder="Họ và tên">
-                <input type="text" placeholder="Số diện thoại">
-                <input type="text" placeholder="Mật khẩu">
+                <input name="user_name" type="text" placeholder="Họ và tên">
+                <input name="phone_number" type="text" placeholder="Số diện thoại">
+                <input name="password" type="text" placeholder="Mật khẩu">
             </div>
 
             <div class="signin__policy">
-                <input type="checkbox">
-                <p>Đồng ý với <a href="">Điều khoản sử dụng</a> và <a href="">Chính sách bảo mật </a>của chúng tôi.</p>
+                <input type="checkbox" id="agreeCheckbox">
+                <p>Đồng ý với <a href="">Điều khoản sử dụng</a> và <a href="">Chính sách bảo mật</a> của chúng tôi.</p>
             </div>
 
-            <a href="./login.php" class="signin__btn">
-                <button type="button">ĐĂNG KÝ</button>
-            </a>
+            
+                <input class="submit_btn" type="submit" name="submit" id="registerBtn" disabled value="Đăng Ký">
 
             <div class="login">
                 <p>Đã có tài khoản? <a href="./login.php">Đăng nhập ngay</a></p>
             </div>
 
-            
-        </div>
+
+        </form>
     </main>
 </body>
+<script>
+    document.getElementById('agreeCheckbox').addEventListener('change', function () {
+        const registerBtn = document.getElementById('registerBtn');
+        registerBtn.disabled = !this.checked; // Disable the button if checkbox is unchecked
+    });
+</script>
 
 </html>
